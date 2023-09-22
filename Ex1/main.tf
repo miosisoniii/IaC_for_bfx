69fc20514ec47aadd993f40380fedb526ede86b1
miosisoniii <miosisoniii@gmail.com>
2023-09-22 16:26:45 -0600

    create one instance

--- Ex1/main.tf
+++ Ex1/main.tf
@@ -1,7 +1,38 @@
 # Ex01 
 
+## Set Provider (AWS)
+provider "aws" {
+    region = "us-west-1"
+}
+
+## Set AWS key pair
+resource "aws_key_pair" "my_key_pair" {
+    key_name = "my-keypair"
+    public_key = file("~/.ssh/id_rsa.pub")
+}
+
+
 ## Push 5 instances
 
+## But first only create one instance
+esource "aws_instance" "docker_instance" {
+  ami             = "ami-06d2c6c1b5cbaee5f"  # Amazon Linux 2 LTS AMI; update if needed
+  instance_type   = "t2.micro"
 
-## Refactor code using loops until matching what is already deployed in 
-AWS
+  key_name        = aws_key_pair.my_keypair.key_name
+
+  tags = {
+    Name = "Ex1-EC2-instance1"
+  }
+
+# use bash to display "hello world" on boot
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, world!" > index.html
+              nohup busybox httpd -f -p 8080 &  # use busybox to 
+              EOF
+}
+
+
+
+## Refactor code using loops until matching what is already deployed in AWS
